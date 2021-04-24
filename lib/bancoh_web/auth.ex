d9ae94d5ccc8382ec77eb213777c5efb1e572524
Vinicius Aguiar <vncsna@gmail.com>
2021-04-23 22:46:10 -0400

    fix: set token to header

--- lib/bancoh_web/auth.ex
+++ lib/bancoh_web/auth.ex
@@ -3,7 +3,8 @@ defmodule BancohWeb.Auth do
 
   def init(opts), do: opts
 
-  def call(%Plug.Conn{body_params: %{"token" => token}} = conn, _opts) do
+  def call(%Plug.Conn{} = conn, _opts) do
+    ["Bearer " <> token] = get_req_header(conn, "authorization")
     case Phoenix.Token.verify(BancohWeb.Endpoint, "userauth", token, max_age: 1800) do
       {:ok, %{user_id: user_id, user_auth: user_auth}} ->
         conn
