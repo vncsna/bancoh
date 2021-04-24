d81d061c582a3fa0cbf001ee0eb4d06e77bec1a3
Vinicius Aguiar <vncsna@gmail.com>
2021-04-24 11:23:43 -0400

    fix: remove default comments

--- lib/bancoh_web/controllers/fallback_controller.ex
+++ lib/bancoh_web/controllers/fallback_controller.ex
@@ -1,20 +1,16 @@
 defmodule BancohWeb.FallbackController do
   @moduledoc """
-  Translates controller action results into valid `Plug.Conn` responses.
-
-  See `Phoenix.Controller.action_fallback/1` for more details.
+  Translates controller action errors into valid `Plug.Conn` responses.
   """
   use BancohWeb, :controller
 
-  # This clause handles errors returned by Ecto's insert/update/delete.
   def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
     conn
     |> put_status(:unprocessable_entity)
     |> put_view(BancohWeb.ChangesetView)
     |> render("error.json", changeset: changeset)
   end
-
-  # This clause is an example of how to handle resources that cannot be found.
+  
   def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
