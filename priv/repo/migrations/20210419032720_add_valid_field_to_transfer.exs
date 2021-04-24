5d92412e0b84ed228d3c35867052861a41dfa7bf
Vinicius Aguiar <vncsna@gmail.com>
2021-04-24 09:12:36 -0400

    fix: rename file

--- priv/repo/migrations/20210419032720_add_valid_field_to_transfer.exs
+++
@@ -1,9 +0,0 @@
-defmodule Bancoh.Repo.Migrations.AddValidFieldToTransfer do
-  use Ecto.Migration
-
-  def change do
-    alter table(:transfers) do
-      add :is_valid, :boolean, default: true
-    end
-  end
-end
